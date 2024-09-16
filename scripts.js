8b3734ba14b58fc6724bfc2d70b0f5f26e25d4b8
Reazul's Code <reazulzannah465@gmail.com>
2024-09-16 12:11:35 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+<script>
+  $(document).ready(function()   
+ {
+    $('.carousel').carousel({
+      interval:   
+ 3000 // Change this value for the transition interval in milliseconds
+    });
+  });
+</script>
+
